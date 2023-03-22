427fd359382c7168cfd4aa6357fbd51e90ffb34e
Kushal <kushal9427@gmail.com>
2023-03-22 20:35:53 +0530

    Type casting and Type checking of 3AC

--- test.java
+++ test.java
@@ -1,33 +1,9 @@
 public class test_3 {
-    public static void main(String[] args)
+    public static void main(int args)
 	{
-
-
-        {
-            int x ;
-        }
-        float b,c ;
-        int a = b + c ;
-        if( a + b) {
-            a = d + e + f;
-            if(false) {
-                char c = 9;
-            }
-        }
-        else {
-            b = dfgth * g;
-        }
-        while(a + b) {
-            c = d - f;
-            c -= a;
-            for(int i=0; i < a; i++) {
-                float k = 1.5;
-                k--;
-                l = - k;
-                t = + o;
-                t++;
-                --o;
-            }
-        }
+        int a,b ;
+        float c,d ;
+        float y = (a + b)*(c + d) ;
+        return ;
 	}
 }
